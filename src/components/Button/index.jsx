e3f9e223d950cb09eb2479f063a94cb3e1170f80
NimfaMargo <Nikiforova.Rita@gmail.com>
2021-02-16 16:10:40 +0300

    refactor existing components

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import 'components/Button/Button.scss';
 
-const Button = ({ children, disabled }) => (
-  <button type="button" className="button" disabled={disabled}>{children}</button>
+const Button = ({ children, disabled, onClick }) => (
+  <button type="button" className="button" onClick={onClick} disabled={disabled}>{children}</button>
 );
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
   disabled: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 
 export default Button;
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import 'components/Card/Card.scss';
 
-const Card = ({ text, url, price }) => (
+const Card = ({ name, url, price }) => (
   <div className="card">
-    <p>{text}</p>
+    <p>{name}</p>
     <p>{url}</p>
     <p>{price}</p>
   </div>
 );
 
 Card.propTypes = {
-  text: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
 };
