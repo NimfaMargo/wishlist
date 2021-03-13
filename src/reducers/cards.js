f5a5dede8d89b304bc5fc1e1bede111caae8da04
NimfaMargo <Nikiforova.Rita@gmail.com>
2021-03-13 16:40:26 +0300

    refactor(store): add edit card actions

--- src/reducers/cards.js
+++ src/reducers/cards.js
@@ -1,14 +1,30 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  list: [],
+  editingCard: null,
+};
+
 const cardsSlice = createSlice({
   name: 'cards',
-  initialState: { list: [] },
+  initialState,
   reducers: {
-    addCard: (state, action) => ({ ...state, list: [...state.list, action.payload] }),
-    removeCard: (state, action) => ({ ...state, list: state.list.filter((el) => el.name !== action.payload) }),
+    addCard: (state, { payload }) => ({ ...state, list: [...state.list, payload] }),
+    updateCard: (state, { payload }) => {
+      const filteredList = state.list.map((el) => (
+        el.id === payload.id ? { id: el.id, ...payload } : el));
+      return { ...state, list: filteredList };
+    },
+    removeCard: (state, { payload }) => (
+      { ...state, list: state.list.filter((el) => el.id !== payload) }),
+    setEditingCard: (state, { payload }) => (
+      { ...state, editingCard: state.list.find((el) => el.id === payload) }),
+    clearEditingCard: (state) => ({ ...state, editingCard: null }),
   },
 });
 
-export const { addCard, removeCard } = cardsSlice.actions;
+export const {
+  addCard, removeCard, updateCard, setEditingCard, clearEditingCard,
+} = cardsSlice.actions;
 
 export default cardsSlice.reducer;
