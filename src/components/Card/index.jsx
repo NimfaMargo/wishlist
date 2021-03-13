430c01aa8bd070188356753a73b6e825eed74b1b
NimfaMargo <Nikiforova.Rita@gmail.com>
2021-03-13 17:27:23 +0300

    feat(card) add confirm dialog for deleting item

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './styles/Card.scss';
-import { useDispatch } from 'react-redux';
-import { removeCard } from 'reducers/cards';
 
 const Card = ({
-  id, name, url, price, onEdit,
-}) => {
-  const dispatch = useDispatch();
-
-  return (
-    <div className="card">
-      {url ? <p><a href={url}>{name}</a></p> : <p>{name}</p>}
-      <p>{price}</p>
-      <button aria-label="edit" className="card__remove" onClick={() => dispatch(removeCard(id))} type="button" />
-      <button aria-label="remove" className="card__edit" onClick={() => onEdit(id)} type="button" />
-    </div>
-  );
-};
+  id, name, url, price, onEdit, onRemove,
+}) => (
+  <div className="card">
+    {url ? <p><a href={url}>{name}</a></p> : <p>{name}</p>}
+    <p>{price}</p>
+    <button aria-label="edit" className="card__remove" onClick={() => onRemove({ name, id })} type="button" />
+    <button aria-label="remove" className="card__edit" onClick={() => onEdit(id)} type="button" />
+  </div>
+);
 Card.propTypes = {
   name: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
--- src/hoc/Layout/index.jsx
+++ src/hoc/Layout/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'hoc/Layout/Layout.scss';
 import Button from 'components/Button';
 import Card from 'components/Card';
@@ -6,16 +6,30 @@ import { useDispatch, useSelector } from 'react-redux';
 import Modal from 'components/Modal';
 import Form from 'components/Form';
 import { hideModal, showModal } from 'reducers/modal';
-import { setEditingCard, clearEditingCard } from 'reducers/cards';
+import { setEditingCard, clearEditingCard, removeCard } from 'reducers/cards';
+import ConfirmDialog from '../../components/ComfirmDialog';
 
 const Layout = () => {
   const dispatch = useDispatch();
+  const [isOpenConfirm, setOpenConfirm] = useState(false);
+  const [deletingCard, setDeletingCard] = useState(null);
+
   const cards = useSelector((state) => state.cards.list);
   const { isModalVisible } = useSelector((state) => state.modal);
 
   const handleCardEdit = (id) => {
     dispatch(setEditingCard(id));
     dispatch(showModal());
+  };
+
+  const cardRemove = () => {
+    setOpenConfirm(false);
+    dispatch(removeCard(deletingCard.id));
+  };
+
+  const confirmCardRemove = (card) => {
+    setOpenConfirm(true);
+    setDeletingCard(card);
   };
 
   const handleModalClose = () => {
@@ -28,7 +42,20 @@ const Layout = () => {
       <div className="layout-main">
         <Button onClick={() => dispatch(showModal())}>Добавить желание</Button>
         <Modal onClose={handleModalClose} isOpen={isModalVisible}><Form /></Modal>
-        {cards.map((card) => <Card {...card} onEdit={(id) => handleCardEdit(id)} />)}
+        <ConfirmDialog
+          onClose={() => setOpenConfirm(false)}
+          isOpen={isOpenConfirm}
+          onSubmit={cardRemove}
+        >
+          {`Вы уверены, что хотите удалить ${deletingCard?.name}?`}
+        </ConfirmDialog>
+        {cards.map((card) => (
+          <Card
+            {...card}
+            onRemove={(item) => confirmCardRemove(item)}
+            onEdit={(id) => handleCardEdit(id)}
+          />
+        ))}
       </div>
     </div>
   );
